6a29400a062a12a1890b35dba4e59d3cad0a68d2
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-22 23:18:21 +0300

    Implement Interfaces  for services. Closes #5

---
+++ pet-clinic-data/src/main/java/gury/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,18 @@
+package gury.springframework.sfgpetclinic.services;
+
+import gury.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+   Owner findById(Long id);
+
+   Owner findByLastName(String lastName);
+
+   Owner save(Owner owner);
+
+   Set<Owner> findAll();
+
+
+}
