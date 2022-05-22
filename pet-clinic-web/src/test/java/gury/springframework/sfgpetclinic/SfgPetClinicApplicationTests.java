48b6abedf083c94b63ff43f3cad6742b7acd2e73
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-22 13:53:27 +0300

    refactoring to multimodulemaven project. closes

--- pet-clinic-web/src/test/java/gury/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/gury/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,9 +3,11 @@ package gury.springframework.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
