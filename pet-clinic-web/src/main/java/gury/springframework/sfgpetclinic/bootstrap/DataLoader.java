b94a81a0b183b2f26bcc578aa9a10d9dafdff523
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-28 15:14:54 +0300

    Refactored Map Impl to abstract ID generation. closes #24

--- pet-clinic-web/src/main/java/gury/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/gury/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -25,28 +25,24 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1=new Owner();
-        owner1.setId(1l);
         owner1.setFirstname("Fedor");
         owner1.setLastname("Sherbanyuk");
         ownerService.save(owner1);
 
 
         Owner owner2=new Owner();
-        owner2.setId(2l);
         owner2.setFirstname("Vladimir");
         owner2.setLastname("Sherbanyuk");
         ownerService.save(owner2);
         System.out.println("Load Owners");
 
         Vet vet1=new Vet();
-        vet1.setId(1l);
         vet1.setFirstname("F");
         vet1.setLastname("S");
         vetService.save(vet1);
 
 
         Vet vet2=new Vet();
-        vet2.setId(2l);
         vet2.setFirstname("V");
         vet2.setLastname("S");
         vetService.save(vet2);
