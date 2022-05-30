1a76df0eda21fb599b9281dfd48aec9cb7adc852
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-30 21:47:19 +0300

    added Specialities Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/gury/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package gury.springframework.sfgpetclinic.services;
+
+import gury.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
