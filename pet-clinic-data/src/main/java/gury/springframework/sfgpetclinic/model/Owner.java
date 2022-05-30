f193d591f2a9f7d3f25e956d39b8229b45e2a9d1
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-30 14:00:00 +0300

    added visits, update owner with pet relationship. closes #27

--- pet-clinic-data/src/main/java/gury/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/gury/springframework/sfgpetclinic/model/Owner.java
@@ -2,6 +2,17 @@ package gury.springframework.sfgpetclinic.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
